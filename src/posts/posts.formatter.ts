f06ed95dd1de0941e7f2f81f899bc291224d81e2
PierreTurnbull <pierre.turnbull42@gmail.com>
2020-03-12 12:22:05 +0100

    Added posts sort.

--- src/posts/posts.formatter.ts
+++ src/posts/posts.formatter.ts
@@ -4,27 +4,29 @@ import { VotesFormatter } from "src/votes/votes.formatter";
 
 export abstract class PostsFormatter {
   static getFormattedPosts (data, currentUserId): PostsDTO[] {
-    return data.map((item) => {
-      const { id, title, content, user, votes } = item;
-      const votesSum = votes.reduce((a, b) => {
-        return a + (b.value ? 1 : - 1);
-      }, 0)
-      const votesYesCount = votes.filter(vote => vote.value).length
-      const votesNoCount = votes.filter(vote => !vote.value).length
-      const votesCount = votes.length
-      const currentUserVote = votes.filter(vote => vote.userId == currentUserId)[0]
-      const formattedPost: PostsDTO = {
-        id,
-        title,
-        content,
-        user: UsersFormatter.getFormattedUser(user),
-        votesSum,
-        votesCount,
-        votesYesCount,
-        votesNoCount,
-        currentUserVote: VotesFormatter.getFormattedVote(currentUserVote) 
-      }
-      return formattedPost;
-    })
+    return data
+      .map((item) => {
+        const { id, title, content, user, votes } = item;
+        const votesSum = votes.reduce((a, b) => {
+          return a + (b.value ? 1 : - 1);
+        }, 0)
+        const votesYesCount = votes.filter(vote => vote.value).length
+        const votesNoCount = votes.filter(vote => !vote.value).length
+        const votesCount = votes.length
+        const currentUserVote = votes.filter(vote => vote.userId == currentUserId)[0]
+        const formattedPost: PostsDTO = {
+          id,
+          title,
+          content,
+          user: UsersFormatter.getFormattedUser(user),
+          votesSum,
+          votesCount,
+          votesYesCount,
+          votesNoCount,
+          currentUserVote: VotesFormatter.getFormattedVote(currentUserVote) 
+        }
+        return formattedPost;
+      })
+      .sort((a, b) => a.votesCount - b.votesCount)
   }
 }
